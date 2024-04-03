ded288752d9d320c527c10b9ec4c34f6025f6133
crative <hannig.sebi@gmail.com>
2024-04-03 18:33:27 +0200

    Updated the ExtendedArmorMaterials, so it uses an Enum instead of different classes. Added a Class to make Full Armor Effects.

--- src/main/java/io/github/crative/extended_armor/items/ExtendedArmorItems.java
+++ src/main/java/io/github/crative/extended_armor/items/ExtendedArmorItems.java
@@ -1,27 +1,25 @@
 package io.github.crative.extended_armor.items;
 
 import io.github.crative.extended_armor.ExtendedArmor;
-import io.github.crative.extended_armor.items.armor_materials.CopperArmorMaterial;
+import io.github.crative.extended_armor.items.armor_materials.ExtendedArmorMaterials;
+import io.github.crative.extended_armor.items.custom.ExtendedArmorItem;
 import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 
 public class ExtendedArmorItems {
-	//* Armor Materials
-	public static final ArmorMaterial COPPER_ARMOMR_MATERIAL = new CopperArmorMaterial();
-
 	//* Copper Armor
 	public static final ArmorItem COPPER_HELMET = registerArmorItem("copper_helmet",
-		new ArmorItem(COPPER_ARMOMR_MATERIAL, ArmorItem.ArmorSlot.HELMET, new QuiltItemSettings()));
+		new ExtendedArmorItem(ExtendedArmorMaterials.COPPER, ArmorItem.ArmorSlot.HELMET, new QuiltItemSettings()));
 	public static final ArmorItem COPPER_CHESTPLATE = registerArmorItem("copper_chestplate",
-		new ArmorItem(COPPER_ARMOMR_MATERIAL, ArmorItem.ArmorSlot.CHESTPLATE, new QuiltItemSettings()));
+		new ArmorItem(ExtendedArmorMaterials.COPPER, ArmorItem.ArmorSlot.CHESTPLATE, new QuiltItemSettings()));
 	public static final ArmorItem COPPER_LEGGINGS = registerArmorItem("copper_leggings",
-		new ArmorItem(COPPER_ARMOMR_MATERIAL, ArmorItem.ArmorSlot.LEGGINGS, new QuiltItemSettings()));
+		new ArmorItem(ExtendedArmorMaterials.COPPER, ArmorItem.ArmorSlot.LEGGINGS, new QuiltItemSettings()));
 	public static final ArmorItem COPPER_BOOTS = registerArmorItem("copper_boots",
-		new ArmorItem(COPPER_ARMOMR_MATERIAL, ArmorItem.ArmorSlot.BOOTS, new QuiltItemSettings()));
+		new ArmorItem(ExtendedArmorMaterials.COPPER, ArmorItem.ArmorSlot.BOOTS, new QuiltItemSettings()));
+
 
 	//* Registry
 	private static ArmorItem registerArmorItem(String name, ArmorItem armorItem){
